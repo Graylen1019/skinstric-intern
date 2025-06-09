2ea4f6c91cf0f9758ee261be54b1a9a6d0ecb6ee
Graylen <delilhabigelow@gmail.com>
2025-06-09 06:13:24 -0400

    added font, created (home) route, and created navbar component in modules.

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "@/modules/navbar";
+
+const Page = () => {
+    return ( 
+        <>
+            <Navbar />
+            <div>
+                
+            </div>
+        </>
+     );
+}
+ 
+export default Page;
---
+++ src/modules/navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <div className="flex flex-row h-[64px] w-full items-center justify-between py-3 mb-3 relative z-[50]">
+      <div className="scale-75 flex flex-row items-center justify-center pt-1">
+        <Link
+          href={"/"}
+          className="flex items-center justify-center gap-2 h-9 px-4 py-2 font-semibold text-sm mr-2 leading-[16px] "
+        >
+          SKINSTRIC
+        </Link>
+        <div className="flex items-center justify-center">
+          <img src="/left-bracket.png" alt="left bracket" />
+          <p className="text-[#1A1B1C83] font-bold text-sm ml-1 mr-1">INTRO</p>
+          <img src="/right-bracket.png" alt="right bracket" />
+        </div>
+      </div>
+      <button className="scale-75 flex items-center justify-center gap-2 font-semibold pointer-events-none h-9 px-4 py-2 mx-4 text-white text-[10px] bg-black ">ENTER CODE</button>
+    </div>
+  );
+};
